ed7e43df0d24beb896dd12a1a23049b1eea3ee03
nouran <nouran.alalami@gmail.com>
2024-05-02 22:46:58 +0400

    ci: test

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 
 import { Chart } from '@/components/details/chart/Chart';
 import { Overview } from '@/components/details/overview/Overview';
-import { COINS } from '@/constants';
 
 export default function Details() {
   return (
@@ -21,11 +20,4 @@ export default function Details() {
       </p>
     </div>
   );
-}
-export async function getStaticPaths() {
-  const paths = COINS.map(id => ({
-    params: { id: id.toString() },
-  }));
-
-  return { paths, fallback: false };
 }
