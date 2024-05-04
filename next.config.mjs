fe8c552decb04b14d1b3575697c6e78a4f078b87
nouran <nouran.alalami@gmail.com>
2024-05-04 20:03:46 +0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  basePath: isProd ? '/real-time-price-tracker' : '',
+  output: 'export',
+  distDir: 'out',
 };
 
 export default nextConfig;
