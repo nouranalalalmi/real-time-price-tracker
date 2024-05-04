b745fde1d655edc951d824d85ebe298ef8c5ab0b
nouran <nouran.alalami@gmail.com>
2024-05-04 19:23:13 +0400

    test: environment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Table } from '@/components/home/Table';
 export default function HomePage() {
   return (
     <div>
+      {process.env.NODE_ENV}
       <h1 className="pb-2 text-3xl font-semibold">Real Time Price Tracker (24hr)</h1>
       <Table />
     </div>
