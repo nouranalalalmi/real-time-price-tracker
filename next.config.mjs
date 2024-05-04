99b0670a373bfb569f35d5e0c78112d0558f08bf
nouran <nouran.alalami@gmail.com>
2024-05-04 19:07:49 +0400

    build: testing image deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  basePath: isProd ? '/real-time-price-tracker' : undefined,
+  basePath: isProd ? '/nouranalalalmi.github.io/real-time-price-tracker' : undefined,
 };
 
 export default nextConfig;
