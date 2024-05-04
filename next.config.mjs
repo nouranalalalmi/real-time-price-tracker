02af79e9a7e167a053e8d13ec5d97b6194882223
nouran <nouran.alalami@gmail.com>
2024-05-04 23:36:20 +0400

    build: fix for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    path: isProd ? '/real-time-price-tracker/' : '/',
   },
-  basePath: isProd ? '/real-time-price-tracker' : undefined,
-  assetPrefix: isProd ? '/real-time-price-tracker/' : '/',
 };
 
 export default nextConfig;
