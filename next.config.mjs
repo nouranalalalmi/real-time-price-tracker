d2c77d4f1e098f8a4e8a511d19f6c0c636998d4c
nouran <nouran.alalami@gmail.com>
2024-05-04 20:54:26 +0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    loader: 'default',
+    path: isProd
+      ? 'https://nouranalalalmi.github.io/real-time-price-tracker'
+      : 'http://localhost:3000',
+  },
+  //   basePath: isProd ? '/real-time-price-tracker' : '',
+  //   assetPrefix: isProd ? '/real-time-price-tracker/' : '/',
+};
 
 export default nextConfig;
